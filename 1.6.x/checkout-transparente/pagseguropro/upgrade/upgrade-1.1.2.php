5bc0cc72106d356d68329e5844b2fd4704b62408
PrestaBR <sac@prestabr.com.br>
2019-11-05 13:02:04 -0300

    PagSeguroPro

---
+++ 1.6.x/checkout-transparente/pagseguropro/upgrade/upgrade-1.1.2.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * 2019 PrestaBR
+ * 
+ * Módulo de Pagamento para Integração com o PagSeguro
+ *
+ * Pagamento com Cartão de Crédito, Boleto e Transferência Bancária
+ * Checkout Transparente - Módulo Oficial - PS 1.6.x
+ *
+ */
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+function upgrade_module_1_1_2($module)
+{
+	$sql_c = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '"._DB_PREFIX_."pagseguropro' AND column_name = 'credencial' AND table_schema = '"._DB_NAME_."'";
+	$credencial = Db::getInstance()->getRow($sql_c);
+	if (!$credencial) {
+		if (!Db::getInstance()->execute("ALTER TABLE `"._DB_PREFIX_."pagseguropro` ADD `credencial` varchar(32) NULL AFTER `url`;")){
+			return false;
+		}
+	}
+	$sql_t = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '"._DB_PREFIX_."pagseguropro' AND column_name = 'token_codigo' AND table_schema = '"._DB_NAME_."'";
+	$token_codigo = Db::getInstance()->getRow($sql_t);
+	if (!$token_codigo) {
+		if (!Db::getInstance()->execute("ALTER TABLE `"._DB_PREFIX_."pagseguropro` ADD `token_codigo` varchar(128) NULL AFTER `credencial`;")){
+			return false;
+		}
+	}
+	$sql_r = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '"._DB_PREFIX_."pagseguropro' AND column_name = 'referencia' AND table_schema = '"._DB_NAME_."'";
+	$referencia = Db::getInstance()->getRow($sql_r);
+	if (!$referencia) {
+		if (!Db::getInstance()->execute("ALTER TABLE `"._DB_PREFIX_."pagseguropro` ADD `referencia` varchar(128) NULL AFTER `id_order`;")){
+			return false;
+		}
+	}
+	$sql_l = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '"._DB_PREFIX_."pagseguropro_logs' AND column_name = 'id_cart' AND table_schema = '"._DB_NAME_."'";
+	$idcart = Db::getInstance()->getRow($sql_l);
+	if (!$idcart) {
+		if (!Db::getInstance()->execute("ALTER TABLE `"._DB_PREFIX_."pagseguropro_logs` ADD `id_cart` int(11) NULL AFTER `id_log`;")){
+			return false;
+		}
+	}
+	Configuration::updateValue('PAGSEGUROPRO_ADDRESS_REQUIRED', 1, false);
+	return true;
+}
+
---
+++ Readme.md
@@ -0,0 +1,32 @@
+![](https://prestabr.com.br/docpagseguropro/master/head_github_master_v4.jpg)
+
+## Instalação - PrestaShop 1.6.x
+
+Extraia o arquivo .ZIP em seu computador e envie a pasta do módulo desejado para o diretório /modules/ no FTP da sua loja.
+
+Em seguida, acesse a Aba Módulos, localize o módulo pesquisando pelo nome e clique no botão "Instalar", exemplo:
+
+![](https://prestabr.com.br/docpagseguropro/16/img01.jpg)
+
+Veja a documentação completa: [PagSeguroPro - Checkout Transparente - PS 1.6.x](https://github.com/pagseguro/prestashop/tree/master/1.6.x/checkout-transparente/pagseguropro)
+
+
+## Instalação - PrestaShop 1.7.x
+
+Extraia o arquivo "prestashop-master.zip" em seu computador:
+
+![](https://prestabr.com.br/docpagseguropro/17/inst01.jpg)
+
+Navegue até o diretório do módulo "/prestashop-master/1.7.x/checkout-transparente/" e crie um arquivo .ZIP do diretório "/pagseguropro/":
+
+![](https://prestabr.com.br/docpagseguropro/17/inst02.jpg)
+
+![](https://prestabr.com.br/docpagseguropro/17/inst03.jpg)
+
+Acesse a Aba "Módulos > Module Manager", clique em "Enviar um módulo", e envie o arquivo "pagseguropro.zip".
+
+![](https://prestabr.com.br/docpagseguropro/17/inst04.jpg)
+
+Pronto, o módulo está instalado!
+
+Veja a documentação completa: [PagSeguroPro - Checkout Transparente - PS 1.7.x](https://github.com/pagseguro/prestashop/tree/master/1.7.x/checkout-transparente/pagseguropro)
