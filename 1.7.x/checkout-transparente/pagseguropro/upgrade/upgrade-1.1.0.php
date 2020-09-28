c79dfcbf640ff21ced72e1d707cc286a8f2aad81
PrestaBR <sac@prestabr.com.br>
2020-09-28 02:20:43 -0300

    PagSeguroPro

---
+++ 1.7.x/checkout-transparente/pagseguropro/upgrade/upgrade-1.1.0.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * 2020 PrestaBR
+ * 
+ * Módulo de Pagamento para Integração com o PagSeguro
+ *
+ * Pagamento com Cartão de Crédito, Boleto e Transferência Bancária
+ * Checkout Transparente - Módulo Oficial - PS 1.7.x
+ *
+ */
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+function upgrade_module_1_1_0($module)
+{
+    Configuration::updateValue('PAGSEGUROPRO_PARCELA_MINIMA', '1.00', false);
+	return true;
+}
+
