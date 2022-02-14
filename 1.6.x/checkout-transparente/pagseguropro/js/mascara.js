f16d2b15c8426d6cb3bf667fe9089d5c3bf4e718
PrestaBR <sac@prestabr.com.br>
2022-02-14 13:58:28 -0300

    PagSeguroPro

---
+++ 1.6.x/checkout-transparente/pagseguropro/js/mascara.js
@@ -0,0 +1,61 @@
+/*
+ * 2011-2022 PrestaBR
+ * 
+ * Módulo de Pagamento para Integração com o PagSeguro
+ *
+ * Pagamento com Cartão de Crédito, Boleto e Transferência Bancária
+ * Checkout Transparente - Módulo Oficial - PS 1.6.x
+ *
+ */
+
+function mascara(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout("execmascara()",1)
+}
+
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value);
+}
+
+function telefone(v){
+    v=v.replace(/\D/g,"")
+    v=v.replace(/^(\d\d)(\d)/g,"($1) $2")
+   	v=v.replace(/(\d)(\d{4})$/,"$1-$2")
+    return v;
+}
+
+function cpfmask(v){
+    v=v.replace(/\D/g,"")
+    v=v.replace(/(\d{3})(\d)/,"$1.$2")
+    v=v.replace(/(\d{3})(\d)/,"$1.$2")
+    v=v.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+    return v;
+}
+
+function cep(v){
+    v=v.replace(/\D/g,"")
+    v=v.replace(/^(\d{5})(\d)/,"$1-$2")
+    return v;
+}
+
+function cnpjmask(v){
+    v=v.replace(/\D/g,"")
+    v=v.replace(/^(\d{2})(\d)/,"$1.$2")
+    v=v.replace(/^(\d{2})\.(\d{3})(\d)/,"$1.$2.$3")
+    v=v.replace(/\.(\d{3})(\d)/,".$1/$2")
+    v=v.replace(/(\d{4})(\d)/,"$1-$2")
+    return v;
+}
+function data(v){
+    v=v.replace(/\D/g,"")
+	v=v.replace(/(\d{2})(\d)/,"$1/$2");
+    v=v.replace(/(\d{2})(\d)/,"$1/$2");
+	v=v.replace(/(\d{2})(\d{2})$/,"$1$2");
+    return v;
+}
+function creditcard(v){
+    v=v.replace(/\D/g,"")
+    v=v.replace(/(.{4})/g, '$1 ')
+    return v;
+}
